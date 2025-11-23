69e64c4f22f6d21173e40a25c278c15802b423fb
ajogious <ajogious@gmail.com>
2025-11-23 12:18:14 +0100

    featured added: PostRepository was tested with test data

--- src/main/java/com/aspacelife/java_assessment/JavaAssessmentApplication.java
+++ src/main/java/com/aspacelife/java_assessment/JavaAssessmentApplication.java
@@ -1,13 +1,27 @@
 package com.aspacelife.java_assessment;
 
+import com.aspacelife.java_assessment.model.Post;
+import com.aspacelife.java_assessment.repository.PostRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JavaAssessmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaAssessmentApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(JavaAssessmentApplication.class, args);
+
+        PostRepository repo = context.getBean(PostRepository.class);
+
+        // this will create and save a test post
+        Post testPost = new Post(999, 1, "Test Title", "Test Body");
+        repo.save(testPost);
+
+        // this is to verify it was saved
+        System.out.println("Total posts in DB: " + repo.count());
+        System.out.println("Saved post: " + repo.findById(999).orElse(null));
 	}
 
 }
