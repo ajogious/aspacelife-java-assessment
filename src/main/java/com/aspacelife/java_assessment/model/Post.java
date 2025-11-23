7c8f0d63a5858b1b6ba1bfcafa6b523a760bd4da
ajogious <ajogious@gmail.com>
2025-11-23 11:51:48 +0100

    features: initialize Spring Boot project with SQLite, base dependencies and Post entity model with JPA annotations implemented

---
+++ src/main/java/com/aspacelife/java_assessment/model/Post.java
@@ -0,0 +1,22 @@
+package com.aspacelife.java_assessment.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    private int userId;
+
+    @Id
+    private int id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String body;
+}
